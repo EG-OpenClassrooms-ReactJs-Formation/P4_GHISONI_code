20ff7e4fe92c11bcc4b6621b2ad70d06c297bf93
Enzo Ghisoni <enzo.ghisoni@hotmail.fr>
2022-01-19 19:57:56 +0100

    Debug the setCustom validity and add red box around radio when invalide

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,40 +11,61 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const crossBtn = document.querySelectorAll(".close")
+const crossBtn = document.querySelectorAll(".close");
 
 // Form data
-const firstNameField = document.getElementById("first")
-const lastNameField = document.getElementById("last")
-const emailField = document.getElementById("email")
-const birthDateField = document.getElementById("birthdate")
-const formSubmitButton = document.getElementsByClassName("btn-submit")
+const firstNameField = document.getElementById("first");
+const lastNameField = document.getElementById("last");
+const emailField = document.getElementById("email");
+const birthDateField = document.getElementById("birthdate");
+const formSubmitButton = document.getElementsByClassName("btn-submit");
 const radios = document.getElementsByClassName('checkbox-input');
 const generalCondition = document.getElementById('checkbox1');
+
+
 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 crossBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-crossBtn.forEach((btn) => btn.addEventListener("click", validate));
+//formSubmitButton.forEach((btn) => btn.addEventListener("click", validate));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  clearForm();
 }
 
 function closeModal() {
+  clearForm();
   modalbg.style.display = "none";
+  
+}
+
+
+function clearForm() {
+
+  firstNameField.value = firstNameField.defaultValue;
+  lastNameField.value = lastNameField.defaultValue;
+  emailField.value = emailField.defaultValue;
+  birthDateField.value = birthDateField.defaultValue;
+  //radios.value = radios.defaultValue;
+  generalCondition.value = generalCondition.defaultValue;
+
 }
 
 function validate() {
   var isEmailValid = emailField.checkValidity();
   var isFirstNameValide = firstNameField.checkValidity();
+  
   var isLastNameValide = lastNameField.checkValidity();
   var isBirthDateValide = birthDateField.checkValidity();
   var isGeneralConditionValide = generalCondition.checked;
   var isOneRadioChecked = false;
+
+
+  console.log(isFirstNameValide);
   for (var i = 0; i < radios.length; i++) {
       if (radios[i].type === 'radio' && radios[i].checked) {
           // get value, set checked flag or do whatever you need to
@@ -59,9 +80,13 @@ function validate() {
   }
   else {
     if(isOneRadioChecked === false){
-      
-      alert("Check a radiobutton");
+      document.getElementsByClassName("formData")[5].style.border = "2px solid #e54858";
+      //alert("Check a radiobutton");
     }
+    else{
+      document.getElementsByClassName("formData")[5].style.border = "0";
+    }
+    //document.getElementsByClassName("formData")[5].style.border = "none";
     console.log("invalide");
     return false;
   }
